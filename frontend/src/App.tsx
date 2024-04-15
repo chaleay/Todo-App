69c9dd6d9af4538d749c1e7737711855fb7ef3e1
Elijah Gaytan <elijahgaytan@utexas.edu>
2024-04-14 20:33:04 -0500

    first

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@mui/material";
+import Button from "@mui/material/Button";
+
+export default function App() {
+  return (
+    <Container>
+      Hello World!
+      <Button variant="contained">Hello as well!</Button>
+    </Container>
+  );
+}
